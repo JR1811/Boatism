e76c345284b9acb27d8a6a2c10fb9849455c22e7
JR1811 <janreichelt77@gmail.com>
2023-12-30 03:14:27 +0100

    added custom engine item and credits

---
+++ src/main/java/net/shirojr/boatism/item/custom/BaseEngineItem.java
@@ -0,0 +1,9 @@
+package net.shirojr.boatism.item.custom;
+
+import net.minecraft.item.Item;
+
+public class BaseEngineItem extends Item {
+    public BaseEngineItem(Settings settings) {
+        super(settings);
+    }
+}
