34f7031b13d9044fdff1a892c59bda791c18a069
ShiroJR <janreichelt77@gmail.com>
2024-02-05 23:13:29 +0100

    renamed destructive explosion naming to be inline with vanilla gamerule names

--- src/main/java/net/shirojr/boatism/gamerule/BoatismGameRules.java
+++ src/main/java/net/shirojr/boatism/gamerule/BoatismGameRules.java
@@ -7,7 +7,7 @@ import net.minecraft.world.GameRules;
 public class BoatismGameRules {
 
     public static final GameRules.Key<GameRules.BooleanRule> DESTRUCTIVE_ENGINE_EXPLOSION =
-            GameRuleRegistry.register("destructive_engine_explosion", GameRules.Category.MISC,
+            GameRuleRegistry.register("engineExplosionDestruction", GameRules.Category.MISC,
                     GameRuleFactory.createBooleanRule(true));
 
     public static void register() {
